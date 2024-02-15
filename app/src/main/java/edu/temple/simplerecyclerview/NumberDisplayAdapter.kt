db3b6172623d7d6f77416b24bc3db0d4beb83b3e
Nicholas Rehac <tup88289@temple.edu>
2024-02-15 11:36:50 -0500

    added lambda parameter and invocation onEntryClick to NumberDisplayAdapter

--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -2,25 +2,30 @@ package edu.temple.simplerecyclerview
 
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
 
 // TODO(Step 1: Implement click behavior)
 
 // TODO (Step 1a: Accept lambda in constructor)
-class NumberDisplayAdapter (private val numbers: Array<Int>) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
+class NumberDisplayAdapter (private val numbers: Array<Int>, private val onEntryClick: (Int)->Unit) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
 
     // TODO (Step 1b: Invoke lambda via onClickListener)
     inner class NumberViewHolder (layout: View) : RecyclerView.ViewHolder (layout) {
         // enumerate views inside layout
         val textView = layout.findViewById<TextView>(R.id.textView)
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
 
         val layout = LayoutInflater.from(parent.context).inflate(R.layout.textview_layout, parent, false)
+        layout.findViewById<TextView>(R.id.textView).setOnClickListener {v: View->
+            val text = v as TextView
+            onEntryClick(text.text.toString().toInt())
+        }
         return NumberViewHolder(layout)
     }
 
